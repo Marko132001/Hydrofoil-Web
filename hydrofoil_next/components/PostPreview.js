2ff9e279495fd32d2a9292a6e839f412d53c508d
Marko132001 <markokozlov@gmail.com>
2022-07-28 12:48:02 +0200

    Responsive title text

--- hydrofoil_next/components/PostPreview.js
+++ hydrofoil_next/components/PostPreview.js
@@ -6,11 +6,19 @@ function PostPreview( {post} ) {
     <>
       <Link href="#">
       <div className="postPreview">
+        <div className="postImage">
           <img src={ `${'http://localhost:1337'}${post.attributes.image.data.attributes.formats.small.url}` } width="400" height="267" alt="image"  />
-          <body>
-            <h3 className="post-title">{post.attributes.title}</h3>    
-            <p className="post-text">{post.attributes.description}</p>
-          </body>
+        </div>
+        <div className="postInfo">
+          <div className="postDate">
+            <span>Sunday </span>
+            <span>July 26 2022</span>
+          </div>
+          <h3 className="post-title">{post.attributes.title}</h3>    
+          <p className="post-text">{post.attributes.description}</p>
+          <a href="#" className="post-cta">Read more</a>
+        </div>
+
       </div>
       </Link>
     </>
