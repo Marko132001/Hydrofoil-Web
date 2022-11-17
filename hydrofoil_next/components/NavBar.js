58e70a76a6f661438ccb53b2500143e20d0c26f9
Marko132001 <markokozlov@gmail.com>
2022-11-17 15:13:29 +0100

    Test navbar

--- hydrofoil_next/components/NavBar.js
+++ hydrofoil_next/components/NavBar.js
@@ -16,7 +16,7 @@ const Logo = () => {
 }
 
 
-
+/*
 function NavBar( {navItems} ) {
 
 
@@ -81,9 +81,66 @@ function NavBar( {navItems} ) {
     </>
   );
 
-
-
 }
 
 export default NavBar;
+*/
 
+function NavBar( {navItems} ) {
+
+
+	const [showMobile, setShowMobile] = React.useState(false);
+
+	return(
+		<>
+			<nav className="bg-gray-100">
+				<div className="px-7 mx-auto">
+					<div className="flex justify-between">
+
+						<div className="flex space-x-4">
+							{/*logo*/}
+							<div>
+								<a href="#" className="flex items-center py-4 px-2 text-gray-700">
+									<Logo />
+									<span className="text-black text-xl sm:text-2xl mr-4 font-semibold">Adria<span className="text-sky-500 hover:text-sky-600">Hydrofoil</span></span>
+								</a>
+							</div>
+							{/*primary nav*/}
+							<div className="hidden md:flex items-center space-x-1">
+								<a href="#" className="py-4 px-3 text-gray-700 hover:text-black text-lg">Features</a>
+								<a href="#" className="py-4 px-3 text-gray-700 hover:text-black text-lg">Pricing</a>
+							</div>
+						</div>
+
+
+						{/*secondary nav*/}
+						<div className="hidden md:flex items-center space-x-1">
+							<a href="#" className="py-4 px-3" text-lg>Login</a>
+							<a href="#" className="py-3 px-3 bg-blue-300 hover:bg-blue-200 
+							text-blue-800 rounded hover:text-blue-700 transition duration-300 text-lg">Signup</a>
+						
+						</div>
+
+						{/*mobile button goes here*/}
+						<div className="md:hidden flex items-center">
+							<button onClick={() => setShowMobile(!showMobile)}>
+								<svg class="w-12 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+								</svg>
+							</button>
+						</div>
+
+					</div>
+				</div>
+
+				{/*mobile menu*/}
+				<div className={showMobile ? "not-hidden md:hidden" : "hidden"}>
+					<a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">Features</a>
+					<a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">Pricing</a>
+				</div>
+			</nav>
+		</>
+	);
+}
+
+export default NavBar;
