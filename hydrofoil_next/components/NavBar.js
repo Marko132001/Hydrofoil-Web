9c444d3c718e3d26e6ba3361217794231ebaa946
Marko Kozlov <marko.kozlov@asseco-see.hr>
2022-11-27 00:04:40 +0100

    Created dropdown solution on navbar -- wip

--- hydrofoil_next/components/NavBar.js
+++ hydrofoil_next/components/NavBar.js
@@ -88,12 +88,13 @@ export default NavBar;
 
 function NavBar( {navItems} ) {
 
+	const [dropdown, setDropdown] = React.useState(false);
 
 	const [showMobile, setShowMobile] = React.useState(false);
 
 	return(
 		<>
-			<nav className="bg-gray-100">
+			<nav className="bg-gray-100 sticky top-0 z-20">
 				<div className="px-7 mx-auto">
 					<div className="flex justify-between">
 
@@ -106,12 +107,34 @@ function NavBar( {navItems} ) {
 								</a>
 							</div>
 							{/*primary nav*/}
-							<div className="hidden md:flex items-center space-x-1">
-								<a href="#" className="py-4 px-3 text-gray-700 hover:text-black text-lg">Features</a>
-								<a href="#" className="py-4 px-3 text-gray-700 hover:text-black text-lg">Pricing</a>
+							<div className="hidden md:block items-center space-x-1">
+							<ul className="flex flex-col p-4 mt-4 md:flex-row items-baseline">
+								<li><a href="#" className="block py-4 px-3 text-gray-700 hover:text-black text-lg">Features</a></li>
+								<li><a href="#" className="block py-4 px-3 text-gray-700 hover:text-black text-lg">Pricing</a></li>
+								<li>
+									<button onClick={() => setDropdown(!dropdown)} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+									{dropdown && (
+									<div className="absolute z-10 font-normal bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+										<ul className="py-1 text-sm text-gray-700 dark:text-gray-400">
+											<li>
+											<a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+											</li>
+											<li>
+											<a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+											</li>
+											<li>
+											<a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+											</li>
+										</ul>
+									</div>
+									)}
+								</li>
+							</ul>
 							</div>
+							
 						</div>
 
+						
 
 						{/*secondary nav*/}
 						<div className="hidden md:flex items-center space-x-1">
@@ -141,6 +164,7 @@ function NavBar( {navItems} ) {
 			</nav>
 		</>
 	);
+  
 }
 
 export default NavBar;
