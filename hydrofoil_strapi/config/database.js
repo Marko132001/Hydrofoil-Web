541dcafe6d1f8069d7f542f5c34a1e8eef98dff9
Marko Kozlov <markokozlov@gmail.com>
2023-03-13 22:58:52 +0100

    Importing and exporting data. Postgres db setup.

--- hydrofoil_strapi/config/database.js
+++ hydrofoil_strapi/config/database.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
+   /*
   connection: {
     client: 'sqlite',
     connection: {
@@ -8,4 +9,17 @@ module.exports = ({ env }) => ({
     },
     useNullAsDefault: true,
   },
+ */
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'adria_hydrofoil'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'March.132001'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+  
 });
