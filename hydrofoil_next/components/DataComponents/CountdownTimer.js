80799215bd023e2ca05ffb944699c9253f91e1a0
Marko132001 <markokozlov@gmail.com>
2023-11-21 00:28:15 +0100

    Added countdown timer

---
+++ hydrofoil_next/components/DataComponents/CountdownTimer.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+function CountdownTimer({t}){
+
+    const [partyTime, setPartyTime] = useState(false);
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const target = new Date("1/1/2024 23:59:59");
+
+        const interval = setInterval(() => {
+        const now = new Date();
+        const difference = target.getTime() - now.getTime();
+
+        const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+        setDays(d);
+
+        const h = Math.floor(
+            (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        setHours(h);
+
+        const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        setMinutes(m);
+
+        const s = Math.floor((difference % (1000 * 60)) / 1000);
+        setSeconds(s);
+
+        if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+            setPartyTime(true);
+        }
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return(
+        <div className="bg-white min-w-screen">
+            <div className="flex items-center justify-center px-5 py-5 pt-20">
+                <div className="text-gray-500 dark:text-gray-400">
+                    <div className="text-6xl text-center grid grid-cols-2 gap-4 sm:grid-cols-4 md:grid-cols-4 xl:grid-cols-4 w-full items-center justify-center">
+                        <div className="w-24 mx-1 p-2 text-white bg-blue-800 rounded-lg">
+                            <div className="font-mono leading-none">{days}</div>
+                            <div className="font-mono uppercase text-sm leading-none">{t("home:days")}</div>
+                        </div>
+                        <div className="w-24 mx-1 p-2 text-white bg-blue-800 rounded-lg">
+                            <div className="font-mono leading-none">{hours}</div>
+                            <div className="font-mono uppercase text-sm leading-none">{t("home:hours")}</div>
+                        </div>
+                        <div className="w-24 mx-1 p-2 text-white bg-blue-800 rounded-lg">
+                            <div className="font-mono leading-none">{minutes}</div>
+                            <div className="font-mono uppercase text-sm leading-none">{t("home:minutes")}</div>
+                        </div>
+                        <div className="w-24 mx-1 p-2 text-white bg-blue-800 rounded-lg">
+                            <div className="font-mono leading-none">{seconds}</div>
+                            <div className="font-mono uppercase text-sm leading-none">{t("home:seconds")}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default CountdownTimer
--- hydrofoil_next/pages/index.js
+++ hydrofoil_next/pages/index.js
@@ -8,8 +8,10 @@ import vector2 from "../images/vector2.png";
 import NavBar from "../components/NavBar";
 import Testimonials from "../components/DataComponents/Testimonials";
 import Footer from "../components/Footer";
+import CountdownTimer from "../components/DataComponents/CountdownTimer";
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import { useTranslation, UseTranslation } from "next-i18next";
+import GridGallery from "../components/DataComponents/GridGallery";
 
 const Vector1 = () => {
   return <Image
@@ -31,6 +33,35 @@ const Vector2 = () => {
           />
 }
 
+const gridGalleryData = {
+    title: "",
+    images: [
+      {
+        text: "MARKETING TEAM",
+        image: {
+          data: {
+            attributes: {
+              ext: ".jpg",
+              url: "https://res.cloudinary.com/hydrofoilweb/image/upload/v1695482488/Malo_vitra_26b903dc98.jpg?updated_at=2023-09-23T15:21:29.428Z"
+            }
+          }
+        }
+      },
+      {
+        text: "ENGINEERING TEAM",
+        image: {
+          data: {
+            attributes: {
+              ext: ".jpg",
+              url: "https://res.cloudinary.com/hydrofoilweb/image/upload/v1695482806/Teredo_Navalis_dc4e77369a.jpg?updated_at=2023-09-23T15:26:47.862Z"
+            }
+          }
+        }
+
+      }
+    ]
+}
+
 
 
 function Home( {posts, home, locale} ) {
@@ -41,9 +72,42 @@ function Home( {posts, home, locale} ) {
     <>
       <NavBar t={t} />
       <Vector1 /><Vector2 />
+      <CountdownTimer t={t}/>
+      <h1 className="text-4xl text-center mt-3 font-extralight">{t("home:rec_title")}</h1>
+      <h1 className="text-xl text-center mt-10 font-extralight">{t("home:contact_us")} <span className="font-bold">uptzri@gmail.com</span></h1>
+      <div className="grid grid-cols-2 gap-1 mb-20 mt-12 text-center">
+        <div
+        className={`relative transition ease-in duration-300 transform`}
+        >
+          <div className="absolute inset-0 z-10 flex transition duration-200 ease-in hover:opacity-0">
+            <div className="absolute inset-0 bg-black opacity-70"></div>
+            <div className="mx-auto text-white z-10 self-center uppercase tracking-widest text-sm leading-8 max-sm:text-xs">
+              <b>{t("home:marketing")}</b> <br/>
+              {t("home:social_media")} <br/>
+              {t("home:web_design")} <br/>
+              {t("home:foto_video")}
+            </div>
+          </div>
+          <img className="w-full h-full" src={process.env.IMAGES_STRAPI_URL + gridGalleryData.images[0].image.data.attributes.url} alt="" />
+        </div>
+        <div
+        className={`relative transition ease-in duration-300 transform`}
+        >
+          <div className="absolute inset-0 z-10 flex transition duration-200 ease-in hover:opacity-0">
+            <div className="absolute inset-0 bg-black opacity-70"></div>
+            <div className="mx-auto text-white z-10 self-center uppercase tracking-widest text-sm leading-8 max-sm:text-xs">
+              <b>{t("home:engineering")}</b> <br/>
+              {t("home:propulsion")} <br/>
+              {t("home:electronics")} <br/>
+              {t("home:shipbuilding")}
+            </div>
+          </div>
+          <img className="w-full h-full" src={process.env.IMAGES_STRAPI_URL + gridGalleryData.images[1].image.data.attributes.url} alt="" />
+        </div>
+      </div>
       <HomeHeader t={t}/>
       <HomeLatestPosts posts={posts} t={t}/>
-      <Testimonials t={t}/>
+      {/*<Testimonials t={t}/>*/}
       <LayoutElements elements={home} />  
       <Footer t={t} />   
     </>
